564cda475df93a9681efde2e37cdcfba1d84fbf8
Prakhar Jain <78018165+Prakhar8003@users.noreply.github.com>
2024-07-28 21:01:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,4 @@ def delete_user(user_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
